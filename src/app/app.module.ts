873a312b5e15e12dc5f1b7a4bf943595d45e6d64
PratapMane <pratap83@gmail.com>
2023-01-13 15:16:56 +0530

    HI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PratapComponent } from './pratap/pratap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PratapComponent
   ],
   imports: [
     BrowserModule,
